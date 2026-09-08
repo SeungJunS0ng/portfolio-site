6143514d2d35530086b5aa5f9b438ddc209516ff
SeungJun_Song <john091122@naver.com>
2026-09-08 09:51:57 +0900

    style: Archive 진입 여백 균형 조정
    - 헤더와 Archive 라벨 사이 여백을 중간값으로 조정

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ function AppLayout() {
 
   useLayoutEffect(() => {
     if (location.pathname.startsWith("/archive")) {
-      const initialScrollTop = location.pathname === "/archive" ? 128 : 0;
+      const initialScrollTop = location.pathname === "/archive" ? 104 : 0;
       window.scrollTo(0, initialScrollTop);
     }
   }, [location.pathname]);
